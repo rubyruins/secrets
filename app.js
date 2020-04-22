3e7c27ee0d0480ffba9455d9e9cf8e523c3569ff
rubyruins <soumya.parekh@somaiya.edu>
2020-04-22 23:50:49 +0530

    added encyption and environment vars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
  
 const app = express();
+const secret = process.env.SECRET;
  
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -12,10 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
